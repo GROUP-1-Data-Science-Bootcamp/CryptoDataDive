9b6e2ee62faf33b62b8785f4151498ce446a3a08
Alireza Doostimehr <alirezadoostimehr@gmail.com>
2023-09-06 20:33:20 +0330

    add github infos finder to important informations

--- important_information.py
+++ important_information.py
@@ -8,11 +8,12 @@ from selenium.webdriver.common.by import By
 top200_path = os.path.join(".", "top200.csv")
 df = pd.read_csv(top200_path)
 links_to_scrap = df["mainlink"].to_list()
-results_df = pd.DataFrame(columns=["symbol", "github", "tags"])
+results_df = pd.DataFrame(columns=["symbol", "github", "langs", "contributors", "tags"])
 
 
 def get_info(driver, link):
     driver.get(link)
+    time.sleep(10)
 
     symbol = driver.find_element(By.CLASS_NAME, "cjeUNx").text
 
@@ -34,7 +35,42 @@ def get_info(driver, link):
     return symbol, href, tags_name
 
 
+def get_github_info_languages(driver, link):
+    driver.get(link)
+    used_languages_box = driver.find_elements(By.XPATH, "//ul[@class='list-style-none']")[1]
+    used_languages = used_languages_box.find_elements(By.XPATH, ".//span[@class='color-fg-default text-bold mr-1']")
+
+    used_languages_names = list()
+    for language in used_languages:
+        language_name = language.get_attribute("innerHTML")
+        if language_name != "Other":
+            used_languages_names.append(language_name)
+    return used_languages_names
+
+
+def get_github_info_contributors(driver, link):
+    driver.get(link)
+    time.sleep(5)
+    contributors_full_list = driver.find_element(By.XPATH,
+                                                 "//ol[@class='contrib-data list-style-none']")
+    contributors_list = (contributors_full_list.find_elements(By.XPATH,
+                                                              ".//li[@class='contrib-person float-left col-6 my-2 pl-2']") +
+                         contributors_full_list.find_elements(By.XPATH,
+                                                              ".//li[@class='contrib-person float-left col-6 my-2 pr-2']"))
+
+    contributor_link_list = list()
+    for contributor in contributors_list:
+        contributor_link = ((contributor.find_element(By.XPATH, ".//span[@class='d-block Box']").
+                             find_element(By.XPATH, ".//h3[@class='border-bottom p-2 lh-condensed']")).
+                            find_element(By.XPATH, ".//a[@class='text-normal']").
+                            get_attribute("href"))
+        contributor_link_list.append(contributor_link)
+
+    return contributor_link_list
+
+
 driver = webdriver.Firefox()
+driver.minimize_window()
 
 while links_to_scrap:
     link = links_to_scrap[0]
@@ -42,10 +78,19 @@ while links_to_scrap:
         symbol, github, tags = get_info(driver, link)
         print(f"Downloaded: {symbol} info")
 
+        langs = get_github_info_languages(driver, github)
+
+        contributors = get_github_info_contributors(driver, github + "/graphs/contributors")
+        print(contributors)
+
+        print(f"Downloaded: {symbol} github info")
+
         row_symbol = symbol if symbol else "unknown"
         new_row = pd.DataFrame({
             "symbol": [row_symbol],
             "github": [github],
+            "langs": [", ".join(langs)],
+            "contrributors": [", ".join(contributors)],
             "tags": [", ".join(tags)]
         })
         results_df = pd.concat([results_df, new_row], ignore_index=True)
@@ -59,6 +104,7 @@ while links_to_scrap:
     if len(links_to_scrap) == 0:
         print("Downloaded all files. Stopping.")
         break
+    break
 
 driver.quit()
 results_df.to_csv("important-information.csv", index=False)
